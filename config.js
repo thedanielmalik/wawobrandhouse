912b18331cf1a6bb0805e4c2e7a0694bab70fedb
thedanielmalik <danielmalikofficial@gmail.com>
2026-09-19 00:08:51 +0100

    Configure Wawo WhatsApp and Instagram feed

--- config.js
+++ config.js
@@ -1,1 +1,1 @@
-window.WAWO_CONFIG={whatsapp:'234YOURNUMBER',beholdFeedId:'',instagramUrl:'https://instagram.com/wawobrandhouse'};
+window.WAWO_CONFIG={whatsapp:'2348108305661',beholdFeedId:'wawobrandhouse',instagramUrl:'https://instagram.com/wawobrandhouse'};
