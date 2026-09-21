d63f7a5c652c818bc33ec8df85c5a417283c0671
thedanielmalik <danielmalikofficial@gmail.com>
2026-09-21 14:41:07 +0100

    Add WhatsApp request link

--- config.js
+++ config.js
@@ -1,1 +1,1 @@
-window.WAWO_CONFIG={whatsapp:'2348108305661',beholdFeedId:'goJWkYdHXYkDlP1wBNOd',instagramUrl:'https://instagram.com/wawobrandhouse'};
+window.WAWO_CONFIG={whatsapp:'2348108305661',whatsappLink:'https://wa.me/message/4DWMXRBSPKV2J1',beholdFeedId:'goJWkYdHXYkDlP1wBNOd',instagramUrl:'https://instagram.com/wawobrandhouse'};
